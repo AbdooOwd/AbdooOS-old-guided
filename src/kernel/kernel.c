38cb8fe6e9139368097653561b4505029ff272a3
AbdooOwd <benari102@gmail.com>
2024-04-01 22:15:08 +0200

    Added a logo  (:

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,12 +1,30 @@
 
-void main_(); // uuhhh for linker?
-void print(const char* message);
-void clear_screen();
+const char* old_logo[3] = {
+    "  _                   __   __ \n",
+    " |_| |_   _|  _   _  |  | |__ \n",
+    " | | |_| |_| |_| |_| |__|  __|"
+};
 
-
+const char* new_logo[6] = {
+    "   ___    _           _                        ______      ______\n",
+    "  | _ |  | |         | |                      |      |    | _____|\n",
+    " | |_| | | |__     __| |    ____     ____    |   __   |  | |____\n",
+    " |  _  | |  _ |   | _  |   | __ |   | __ |   |  |__|  |   |____ |\n",
+    " | | | | | |_| | | |_| |  | |__| | | |__| |  |        |   _____| |\n",
+    " |_| |_| |____|   |____|   |____|   |____|    |______|   |______|"
+};
 
 void main_() {
+
     print(" > Kernel Entered Successfully!\n");
+
+    for (int i = 0; i < 6; i++) {
+        print((char*)new_logo[i]);
+    }
+
+    print("\n\n");
+
     print(" - Welcome to AbdooOS!\n");
+
     return;
 }
