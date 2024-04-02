d9c91c2cdd3f7b28ecec27e58b9d9223ff38a3ca
AbdooOwd <benari102@gmail.com>
2024-04-02 15:05:41 +0200

    reverted to default entry

--- src/boot/kernel/kernel_entry.asm
+++ src/boot/kernel/kernel_entry.asm
@@ -1,13 +1,8 @@
+%define ENDL 0x0A
+
 section .text
     [bits 32]
-
     [extern _main_]
-    [extern _print]
-
-    mov edx, idiot
     
-    call _print
     call _main_
     jmp $
-
-idiot: db "HELLO", 0
