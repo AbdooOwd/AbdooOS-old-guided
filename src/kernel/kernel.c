dc68f8212c0a55968a53f1ba771af4591ca6b2ec
AbdooOwd <benari102@gmail.com>
2024-04-02 15:06:01 +0200

    Refactoring?

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,11 +1,17 @@
+// #include "./drivers/screen.h"
+// #include "util.h"
 
-const char* old_logo[3] = {
+/* Reboots System by going back to the beginning of boot sector (WIP) */
+extern void reboot_system();
+
+
+const char* small_logo[3] = {
     "  _                   __   __ \n",
     " |_| |_   _|  _   _  |  | |__ \n",
     " | | |_| |_| |_| |_| |__|  __|\n"
 };
 
-const char* new_logo[6] = {
+const char* thick_logo[6] = {
     "   ___    _           _                        ______      ______\n",
     "  | _ |  | |         | |                      |      |    | _____|\n",
     " | |_| | | |__     __| |    ____     ____    |   __   |  | |____\n",
@@ -14,27 +20,13 @@ const char* new_logo[6] = {
     " |_| |_| |____|   |____|   |____|   |____|    |______|   |______|\n"
 };
 
-void print_char(char c, int col, int row);
-void print_at(char* message, int col, int row);
-void print(char* message);
-void clear_screen();
-void reboot();
-
 void main_() {
 
     for (int i = 0; i < 6; i++) {
-        print((char*)new_logo[i]);
+        print((char*)thick_logo[i]);
     }
 
     print("\n\n");
 
     print(" - Welcome to AbdooOS!\n");
-
-    return;
-}
-
-// maybe it works?
-void reboot() {
-    print("\n ] Rebooting...\n");
-    goto * 0x7c00;
 }
