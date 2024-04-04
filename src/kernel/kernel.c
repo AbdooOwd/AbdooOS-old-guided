93fb33fb15940f3b8c6ee5a31011f262e7ce3a75
AbdooOwd <benari102@gmail.com>
2024-04-04 16:14:41 +0200

    minor fixes + wip commands

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -26,12 +26,14 @@ const char* thick_logo[6] = {
 
 const char* help_message[] = {
     "Usage: help",
-    "\nAvailable commands:",
+    "Available commands:\n",
     "ping: just prints \"Pong!\" back",
     "end: stops the CPU",
     "reboot: restarts the kernel",
     "qwerty | azerty: changes the keyboard layout to one of them"
 };
+
+char* previous_command = '\0';
 
 
 void kernel_main() {
@@ -61,7 +63,17 @@ void user_input(char* input) {
     print("\n> ");
 }
 
+void execute_command(char* le_command) {
+    if (strcmp(le_command, "ping") == 0) {
+        print("Pong!");
+    }
+}
+
 int check_action(char* le_input) {
+
+    if (!has_char(le_input, ' '))
+        previous_command = le_input;
+
     if (strcmp(le_input, "ping") == 0) {
         print("Pong!");
         return 1;
@@ -84,11 +96,19 @@ int check_action(char* le_input) {
         // doesn't really reboot
         // goto * 0x0;
         kernel_main();
+
+        // silly thing to remove that little "> " that will stay
+        backspaces(le_input, 3);
+        print_backspaces(3);
         return 1;
     }
     else if ((strcmp(le_input, "qwerty") == 0) || (strcmp(le_input, "azerty") == 0)) {
         change_layout(le_input);
         print("Switched layout.");
+        return 1;
+    }
+    else if (strcmp(le_input, "test") == 0) {
+        print("Test");
         return 1;
     }
     return 0;
