d1630eafd8d5848822d0d3e23341f24af2a07de3
AbdooOwd <benari102@gmail.com>
2024-04-06 14:17:01 +0200

    Some FAT12 stuff

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -1,43 +1,79 @@
 [org 0x7c00]
 KERNEL_LOCATION equ 0x1000
 
-mov [BOOT_DISK], dl
+;
+; FAT12 header
+; 
+jmp short start
+nop
 
-xor ax, ax
-mov es, ax
-mov ds, ax
-mov bp, 0x8000
-mov sp, bp
+bdb_oem:                    db 'MSWIN4.1'           ; 8 bytes
+bdb_bytes_per_sector:       dw 512
+bdb_sectors_per_cluster:    db 1
+bdb_reserved_sectors:       dw 1
+bdb_fat_count:              db 2
+bdb_dir_entries_count:      dw 0E0h
+bdb_total_sectors:          dw 2880                 ; 2880 * 512 = 1.44MB
+bdb_media_descriptor_type:  db 0F0h                 ; F0 = 3.5" floppy disk
+bdb_sectors_per_fat:        dw 9                    ; 9 sectors/fat
+bdb_sectors_per_track:      dw 18
+bdb_heads:                  dw 2
+bdb_hidden_sectors:         dd 0
+bdb_large_sector_count:     dd 0
 
+; extended boot record
+ebr_drive_number:           db 0                    ; 0x00 floppy, 0x80 hdd, useless
+                            db 0                    ; reserved
+ebr_signature:              db 29h
+ebr_volume_id:              db 12h, 34h, 56h, 78h   ; serial number, value doesn't matter
+ebr_volume_label:           db 'ABDOO    OS'        ; 11 bytes, padded with spaces
+ebr_system_id:              db 'FAT12   '           ; 8 bytes
 
-mov bx, KERNEL_LOCATION
-mov dh, 54 ; in zeroes.asm we write about 54 sectors worth of bytes
+; ur code (;)
 
-mov ah, 0x02
-mov al, dh
-mov ch, 0x00
-mov dh, 0x00
-mov cl, 0x02
-mov dl, [BOOT_DISK]
-int 0x13                ; no error management, do your homework!
+start:
+    mov [BOOT_DISK], dl
+    jmp stack_up
 
+stack_up:
+    xor ax, ax
+    mov es, ax
+    mov ds, ax
+    mov bp, 0x8000
+    mov sp, bp
+    jmp kernel_up
 
-mov ah, 0x0
-mov al, 0x3
-int 0x10                ; text mode
+kernel_up:
+    mov bx, KERNEL_LOCATION
+    mov dh, 54 ; in zeroes.asm we write about 54 sectors worth of bytes
+
+    mov ah, 0x02
+    mov al, dh
+    mov ch, 0x00
+    mov dh, 0x00
+    mov cl, 0x02
+    mov dl, [BOOT_DISK]
+    int 0x13                ; no error management, do your homework!
+
+    mov ah, 0x0
+    mov al, 0x3
+    int 0x10                ; text mode
+
+    jmp protecc
 
 
 CODE_SEG equ GDT_code - GDT_start
 DATA_SEG equ GDT_data - GDT_start
 
-cli
-lgdt [GDT_descriptor]
-mov eax, cr0
-or eax, 1
-mov cr0, eax
-jmp CODE_SEG:start_protected_mode
+protecc:
+    cli
+    lgdt [GDT_descriptor]
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+    jmp CODE_SEG:start_protected_mode
 
-jmp $
+    jmp $
 
 BOOT_DISK: db 0
 
