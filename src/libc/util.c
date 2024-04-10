b882c75ceaf682860ef02711c7f2ccddda1871be
AbdooOwd <benari102@gmail.com>
2024-04-10 22:04:04 +0200

    Added 'arary_has_string' func

--- src/libc/util.c
+++ src/libc/util.c
@@ -1,5 +1,14 @@
+#include "util.h"
+
 void array_copy(char source[], char destination[], int size) {
     for (int i = 0; i < size; i++) {
         destination[i] = source[i];
     }
+}
+
+bool array_has_string(char* str_arr[], char* the_str) {
+    for (size_t i = 0; i < sizeof(str_arr); i++) {
+        if (str_arr[i] == the_str) return true;
+    }
+    return false;
 }
--- src/libc/util.h
+++ src/libc/util.h
@@ -1,1 +1,9 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include "../kernel/core/types.h"
+
 void array_copy(char source[], char destination[], int size);
+bool array_has_string(char* str_arr[], char* the_str);
+
+#endif
